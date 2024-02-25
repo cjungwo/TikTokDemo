fece39b1e90237f5d8dc8abd2335273d7d390249
Cjungwo <jwoo516@naver.com>
2024-02-25 21:28:13 +1100

    Initial Commit

---
+++ TikTokDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TikTokDemo
+//
+//  Created by JungWoo Choi on 25/2/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TikTokDemo/TikTokDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokDemoApp.swift
+//  TikTokDemo
+//
+//  Created by JungWoo Choi on 25/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct TikTokDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
